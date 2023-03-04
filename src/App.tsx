eadccdcbb8df85033b2ecdf86f031da4c1e298b4
Elias Rangel <propstle@gmail.com>
2023-03-03 22:34:10 -0600

    threejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as THREE from "three";
+const AMOUNT = 6;
+
+const ASPECT_RATIO = window.innerWidth / window.innerHeight;
+
+const WIDTH = (window.innerWidth / AMOUNT) * window.devicePixelRatio;
+const HEIGHT = (window.innerHeight / AMOUNT) * window.devicePixelRatio;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    1,
+    1000
   );
+
+  scene.add(camera);
+  return <div className="App"></div>;
 }
 
 export default App;
