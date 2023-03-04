c10ffa6c5dbdb40f71c207ebecf8bc9b6ae5cf50
Elias Rangel <propstle@gmail.com>
2023-03-04 15:18:15 -0600

    45

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import * as THREE from "three";
 import useWindowMeasurements from "./hooks/useWindowMeasurements";
+import { useFrame } from "@react-three/fiber";
+function Box(props: any) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef<any>();
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (ref.current!.rotation.x += delta));
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
 
 function App() {
-  const { ASPECT_RATIO, WIDTH, HEIGHT } = useWindowMeasurements(6);
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(45, ASPECT_RATIO, 1, 1000);
-
-  scene.add(camera);
-  return <div className="App"></div>;
+  return <div></div>;
 }
 
 export default App;
